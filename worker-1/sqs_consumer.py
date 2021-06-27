449596d9e6b18f12531184f9d1fc8a0eec01af79
Althaf <althaf1088@gmail.com>
2021-06-27 15:11:53 +0800

    create bucket in s3 from worker

--- worker-1/sqs_consumer.py
+++ worker-1/sqs_consumer.py
@@ -6,13 +6,16 @@ sqs_queue= sqs.get_queue_by_name(QueueName="sgx")
 
 def process_message(message_body):
     print(f"processing message: {message_body}")
-    # do what you want with the message here
-    pass
+    try:
+        s3 = boto3.resource('s3')
+        s3.create_bucket(Bucket=message_body["user"])
+    except Exception as e:
+        print(e)
+
 
 if __name__ == "__main__":
     while True:
         messages = sqs_queue.receive_messages()
         for message in messages:
             process_message(message.body)
-            print(message)
             message.delete()
